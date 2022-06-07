da0ab3cbeff21da8fb75d3dac2cfb6913cf6c211
Murilo Alves <101125668+Murilo2412@users.noreply.github.com>
2022-06-07 10:06:10 -0300

    Initial commit

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = ['1234324341', '4235313213', '324325423423']
+
+//const cliente = ['nome', 'Murilo', 'idade', 22]
+
+const cliente = {
+    nome:'Murilo',
+    idade: 22,
+    cpf: '123454632423',
+    email: 'murilo@email.com'
+}
